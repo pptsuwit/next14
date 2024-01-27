ecae3cd4c6abc7497e9a01b64147aca7d4e42499
t <pptsuwit@gmail.com>
2024-01-27 18:01:10 +0700

    fix: nav, drawer

--- components/Layouts/main/Navbar.tsx
+++ components/Layouts/main/Navbar.tsx
@@ -1,192 +1,146 @@
 "use client";
 import { useGlobalContext } from "@/contexts/store";
+import { AiOutlineMenu, AiOutlinePicture } from "react-icons/ai";
+import defaultImage from "@/assets/avatar.jpg";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import Link from "next/link";
+import { deleteCookie } from "cookies-next";
 const Navbar = () => {
   const { drawer, setDrawer } = useGlobalContext();
-  function handleClick() {
-    console.log("clicked");
+  const [profileImage, setProfileImage] = useState(defaultImage.src);
+  const [menu, setMenu] = useState(false);
+  const name = "t3t";
+  const email = "github.com/pptsuwit";
+
+  const ref = useRef<any>(null);
+  function handleClickOutside(event: MouseEvent) {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setMenu(false);
+    }
   }
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    // return () => {
+    //   // Unbind the event listener on clean up
+    //   document.removeEventListener("mousedown", handleClickOutside);
+    // };
+  }, []);
+
+  const menuList = [
+    { name: "Home", link: "/" },
+    { name: "Profile", link: "https://github.com/pptsuwit" },
+    {
+      name: "Repository",
+      link: "https://github.com/pptsuwit?tab=repositories",
+    },
+    {
+      name: "Dashboard",
+      link: "/dashboard",
+    },
+  ];
+  const liMenuList = menuList.map((item, index) => {
+    return (
+      <li key={index}>
+        <Link
+          onClick={() => setMenu(false)}
+          href={item.link}
+          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+        >
+          {item.name}
+        </Link>
+      </li>
+    );
+  });
+
+  function signOut() {
+    localStorage.removeItem(process.env.TOKEN_NAME as string);
+    deleteCookie(process.env.TOKEN_NAME as string);
+    window.location.reload();
+  }
+  useEffect(() => {
+    // setProfileImage("https://picsum.photos/200/300.jpg");
+    // setProfileImage("https://picsum.photos/id/237/200/300.jpg");
+  }, []);
 
   return (
     <>
       <nav className="bg-white border-gray-200 shadow-md">
-        <div className=" w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ">
-          <div className="flex">
-            <button
-              onClick={() => setDrawer(!drawer)}
-              data-collapse-toggle="navbar-hamburger"
-              type="button"
-              className="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 mr-5"
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
+        {/* <div className="w-screen-xl flex flex-wrap items-center justify-between mx-auto py-2 "> */}
+        <div className="w-screen-xl flex items-center  py-2 ">
+          <div className="flex xs:w-16 sm:w-64 px-2">
+            <div>
+              <button
+                onClick={() => setDrawer(!drawer)}
+                type="button"
+                className="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none mr-5"
               >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
-              </svg>
-            </button>
-            <a
-              href="https://flowbite.com/"
-              className="flex items-center space-x-3 rtl:space-x-reverse"
-            >
-              <img
-                src="https://flowbite.com/docs/images/logo.svg"
-                className="h-8"
-                alt="Flowbite Logo"
-              />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                Flowbite
-              </span>
-            </a>
+                <span className="sr-only">Open main menu</span>
+                <AiOutlineMenu fontSize={40} />
+              </button>
+            </div>
+            <div className="xs:hidden sm:block">
+              <Link
+                href="https://github.com/pptsuwit"
+                className="flex items-center space-x-3 rtl:space-x-reverse"
+              >
+                <AiOutlinePicture fontSize={40} className="text-theme-500" />
+                <span className="self-center text-2xl text-theme-500 font-semibold whitespace-nowrap  ">
+                  Logo
+                </span>
+              </Link>
+            </div>
           </div>
-
-          <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <div className="flex xs:w-[calc(100%-8rem)] sm:w-[calc(100%-20rem)] px-8 xs:justify-center sm:justify-start">
+            <span className="text-3xl">Title</span>
+          </div>
+          <div className="flex xs:w-16 sx:w-16  items-center">
             <button
               type="button"
-              className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-theme-500"
-              id="user-menu-button"
-              aria-expanded="false"
-              data-dropdown-toggle="user-dropdown"
-              data-dropdown-placement="bottom"
-              onClick={handleClick}
+              className="flex text-sm rounded-full ring-2 ring-theme-500"
+              onClick={() => setMenu(!menu)}
             >
-              <span className="sr-only">Open user menu</span>
-              <img
-                className="w-8 h-8 rounded-full"
-                src="/docs/images/people/profile-picture-3.jpg"
-                alt="user photo"
-              />
+              <span className="sr-only">Open menu</span>
+              <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full ">
+                <Image
+                  unoptimized={true}
+                  priority={true}
+                  loader={() => profileImage}
+                  src={profileImage}
+                  // width={500}
+                  // height={500}
+                  fill={true}
+                  alt="avatar"
+                />
+              </div>
             </button>
             {/* <!-- Dropdown menu --> */}
-            <div
-              className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-              id="user-dropdown"
-            >
-              <div className="px-4 py-3">
-                <span className="block text-sm text-gray-900 dark:text-white">
-                  Bonnie Green
-                </span>
-                <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
-                  name@flowbite.com
-                </span>
-              </div>
-              <ul className="py-2" aria-labelledby="user-menu-button">
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                  >
-                    Dashboard
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                  >
-                    Settings
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                  >
-                    Earnings
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                  >
-                    Sign out
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <button
-              data-collapse-toggle="navbar-user"
-              type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="navbar-user"
-              aria-expanded="false"
-              onClick={handleClick}
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
+            {menu && (
+              <div
+                ref={ref}
+                className="absolute top-11 right-5  my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow "
               >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
-              </svg>
-            </button>
-          </div>
-          <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-            id="navbar-user"
-          >
-            {/* <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-white bg-theme-700 rounded md:bg-transparent md:text-theme-700 md:p-0 md:dark:text-theme-500"
-                  aria-current="page"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-theme-700 md:p-0 dark:text-white md:dark:hover:text-theme-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-theme-700 md:p-0 dark:text-white md:dark:hover:text-theme-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Services
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-theme-700 md:p-0 dark:text-white md:dark:hover:text-theme-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Pricing
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-theme-700 md:p-0 dark:text-white md:dark:hover:text-theme-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Contact
-                </a>
-              </li>
-            </ul> */}
+                <div className="px-4 py-3 ">
+                  <span className="block text-sm text-theme-500 font-bold">
+                    {name}
+                  </span>
+                  <span className="block text-sm  text-gray-500 truncate font-bold">
+                    {email}
+                  </span>
+                </div>
+                <ul className="py-2" aria-labelledby="user-menu-button">
+                  {liMenuList}
+                  <li>
+                    <a
+                      onClick={signOut}
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    >
+                      Sign out
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
         </div>
       </nav>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        xs: "0px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
     },
   },
   plugins: [],
